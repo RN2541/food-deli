f111bcea73731d317bb604ff93c5c20cb2e84060
ice <icekong0906@gmail.com>
2023-11-21 16:06:13 +0700

    feat: :zap: edit/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,31 @@
-export default function Home (){
-  return <main className="h-screen w-full flex overflow-scroll">
-    <div className="w-full h-full  pt-6 px-6 space-y-6">
-      <div className="w-full h-auto space-x-4"> Test</div>
-      <div className="w-full h-[200px]  bg-white rounded-3xl"> Test 2</div>
-      <div className="w-full h-[400px] bg-indigo-400">Test3</div>
-      <div className="w-full h-auto flex flex-wrap gap-4">
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-    </div>
-    </div>
-    <div className="w-[500px] h-full bg-white flex flex-col"></div>
-  
-  </main>
-//   return(
-//     <main className="h-screen w-full flex">
-//       <div className="w-full h-full overflow-scroll pt-6 px-6 space-y-6">
-//         <div className="w-full h-auto space-x-4">
-//         </div>
-//         {/* <div className="w-full h-[200px]  bg-white rounded-3xl"></div>
-//         <div className="w-full h-[400px] bg-indigo-400"></div>
-//         <div className="w-full h-auto flex flex-wrap gap-4">
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//           <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
-//         </div> */}
-//       </div>
-//       <div className="w-[500px] h-full bg-white flex flex-col">
-//       <div className="w-full h-[200px] bg-indigo-200"></div>
-//       <div className="w-full h-full bg-slate-400"></div>
-//       <div className="w-full h-[400px] bg-indigo-400"></div>
-//       </div>
-//     </main>
-// )
+export default function Home() {
+    return (
+        <main className="h-screen w-full flex overflow-scroll">
+            <div className="w-full h-full  pt-6 px-6 space-y-6">
+                <div className="w-full h-auto space-x-4"> Test</div>
+                <div className="w-full h-[200px]  bg-white rounded-3xl">
+                    {" "}
+                    Test 2
+                </div>
+                <div className="w-full h-[400px] bg-indigo-400">Test3</div>
+                <div className="w-full h-auto flex flex-wrap gap-4">
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                    <div className="h-[200px] w-[calc(33.33%-16px)] bg-slate-50"></div>
+                </div>
+            </div>
+            <div className="w-[500px] h-full bg-white flex flex-col">
+                <div className="w-full h-[200px] bg-indigo-200"></div>
+                <div className="w-full h-full bg-slate-400"></div>
+                <div className="w-full h-[400px] bg-indigo-400"></div>
+            </div>
+        </main>
+    );
 }
